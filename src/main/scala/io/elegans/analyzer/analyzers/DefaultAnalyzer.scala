1155ee39dde36c93ddf78d4a70aa1e50474a21b1
Angelo Leto <angleto@gmail.com>
2021-03-20 11:39:53 +0100

    fixed tests

--- src/main/scala/io/elegans/analyzer/analyzers/DefaultAnalyzer.scala
+++ src/main/scala/io/elegans/analyzer/analyzers/DefaultAnalyzer.scala
@@ -4,11 +4,16 @@ package io.elegans.analyzer.analyzers
   * Created by mal on 20/02/2017.
   */
 
-import io.elegans.analyzer.atoms._
+import io.elegans.analyzer.atoms.DefaultFactoryAtomic
 import io.elegans.analyzer.operators._
 
-class DefaultAnalyzer(command: String, restrictedArgs: Map[String, String]) extends
-  DefaultParser(command: String, restrictedArgs: Map[String, String]) {
-  override val atomicFactory = new DefaultFactoryAtomic
-  override val operatorFactory = new DefaultFactoryOperator
-}
+abstract class DefaultAnalyzerEarlyInit(command: String,
+                            restrictedArgs: Map[String, String],
+                            override val atomicFactory: DefaultFactoryAtomic,
+                            override val operatorFactory: DefaultFactoryOperator)
+  extends DefaultParser(command: String, restrictedArgs: Map[String, String])
+
+class DefaultAnalyzer(command: String,
+                       restrictedArgs: Map[String, String]) extends
+  DefaultAnalyzerEarlyInit(command,
+     restrictedArgs, new DefaultFactoryAtomic, new DefaultFactoryOperator)

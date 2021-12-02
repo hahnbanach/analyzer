4522f2379486499c68c50ef0e20ea3a2bf7b0814
Angelo Leto <angleto@gmail.com>
2021-12-02 18:09:45 +0100

    added None argument handling

--- src/main/scala/com/hahnbanach/analyzer/atoms/CountStateInHistoryAtomic.scala
+++ src/main/scala/com/hahnbanach/analyzer/atoms/CountStateInHistoryAtomic.scala
@@ -13,29 +13,31 @@ class CountStateInHistoryAtomic(val arguments: List[String],
   override def toString: String = "countStateInHistoryAtomic"
   override val factoryName: String = "countStateInHistoryAtomic"
   val isEvaluateNormalized: Boolean = false
-  override val argumentsDescription: Map[String, (String, Option[String])] = Map(
+  override val argumentsDescription: Map[String, (String, Boolean, Option[String])] = Map(
     //name -> explanation, default (if provided then arg is optional)
     "state" -> (
       "state name to count",
+      true,
       None),
     "maxContext" -> (
       "restrict the count to a limited amount of states starting from the end of the history," +
         "if not provided count on the entire history",
-      "".some)
+      false,
+      None)
   )
 
-  val state: Map[String, String] => String = parameter("state")
-  val maxContext: Map[String, String] => String = parameter("maxContext")
+  val state: Map[String, String] => String = mandatoryParameter("state")
+  val maxContext: Map[String, String] => Option[String] = optParameter("maxContext")
 
   private[this] val nRegex: Regex = "^([0-9]+)$".r
 
   def evaluate(query: String, data: AnalyzersData = AnalyzersData()): Result = {
     val stateName = state(data.stateData.variables)
-    val maxCtxtSize = maxContext(data.stateData.variables) match {
+    val maxCtxSize = maxContext(data.stateData.variables).getOrElse("") match {
       case nRegex(value) => value.toInt
       case _ => data.stateData.variables.size
     }
-    val count = data.stateData.traversedStates.reverse.take(maxCtxtSize).count(s => {
+    val count = data.stateData.traversedStates.reverse.take(maxCtxSize).count(s => {
       s.state === stateName
     })
     Result(count, data)
--- src/main/scala/com/hahnbanach/analyzer/atoms/KeyValueArgsBaseAtomic.scala
+++ src/main/scala/com/hahnbanach/analyzer/atoms/KeyValueArgsBaseAtomic.scala
@@ -1,20 +1,21 @@
 package com.hahnbanach.analyzer.atoms
 
+import cats.implicits._
+
 import scala.util.matching.Regex
 
 case class BaseAtomicException(message: String = "", cause: Throwable = None.orNull)
   extends Exception(message, cause)
 
-//TODO: move into the analyzer project
 trait KeyValueArgsBaseAtomic {
   val arguments: List[String]
   val factoryName: String
-  val argumentsDescription: Map[String, (String, Option[String])]
+  val argumentsDescription: Map[String, (String, Boolean, Option[String])]
 
   def usage: String = s"""$factoryName: accept a sequence of <param name>, <param value>
                          | Hence the number of arguments must be even.
                          | Accepted arguments:
-                         | ${argumentsDescription.map{case(k, v) => s"\t$k (${if(v._2.nonEmpty) "Optional" else "Mandatory"}): ${v._1}"}.mkString("\r")}
+                         | ${argumentsDescription.map{case(k, v) => s"\t$k (${if(v._2) s"Optional with default(${v._3})" else "Mandatory"}): ${v._1}"}.mkString("\r")}
                          | """.stripMargin
 
   private[this] val varNameRegex: Regex =
@@ -45,31 +46,42 @@ trait KeyValueArgsBaseAtomic {
     }).toMap
   }
 
-  def getInputVariable(variableName: String, default: Option[String]): Map[String, String] => String = {
-    def input(varMap: Map[String, String]): String = {
-      (varMap.get(variableName), default) match {
-        case (Some(v), _) => v
-        case (None, Some(d)) => d
-        case (_, _) => throw BaseAtomicException(s"$factoryName: variable $variableName does not exists or no default value provided")
+  def getInputVariable(variableName: String, mandatory: Boolean,
+                       default: Option[String]): Map[String, String] => Option[String] = {
+    def input(varMap: Map[String, String]): Option[String] = {
+      (varMap.get(variableName), mandatory, default) match {
+        case (Some(v), _, _) => v.some
+        case (None, true, Some(_)) => default
+        case (None, false, _) => None
+        case (_, _, _) => throw BaseAtomicException(s"$factoryName: variable $variableName does not exists or no default value provided")
       }
     }
     input
   }
 
-  def parameter(argName: String): Map[String, String] => String = {
+  def mandatoryParameter(argName: String): Map[String, String] => String = {
+    optParameter(argName).map(_.getOrElse(
+      throw BaseAtomicException(s"Missing mandatory argument in $factoryName: $argName")
+    ))
+  }
+
+  def optParameter(argName: String): Map[String, String] => Option[String] = {
     argumentsDescription.get(argName) match {
       case Some(argDescription) =>
         argMap.get (argName) match {
           case Some (p) =>
             if (p._1) { // is variable name
-              getInputVariable(p._2, argDescription._2)
+              getInputVariable(p._2, argDescription._2, argDescription._3)
             } else { // is static argument
-              (_: Map[String, String]) => p._2 // is the value
+              (_: Map[String, String]) => p._2.some // is the value
             }
           case _ =>
             (_: Map[String, String]) =>
-              argDescription._2
-                .getOrElse(throw BaseAtomicException(s"Missing mandatory argument in $factoryName: $argName"))
+              argDescription match {
+                case (_, true, Some(v)) => v.some
+                case (_, false, v) => v
+                case (_, _, _) => throw BaseAtomicException(s"Missing mandatory argument in $factoryName: $argName")
+              }
         }
       case _ => throw BaseAtomicException(s"Argument not supported in $factoryName: $argName")
     }
